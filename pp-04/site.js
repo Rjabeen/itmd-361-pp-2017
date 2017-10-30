4025362df1f6d4345a451e887b374b35dfd5fd37
Reshma Jabeen <31253728+Rjabeen@users.noreply.github.com>
2017-10-30 17:24:02 -0500

    commented console log of undefined variable i

--- pp-04/site.js
+++ pp-04/site.js
@@ -19,7 +19,7 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  like `window.x`):
 
 (function(){
-  
+
   var x = 5;
 
   function double(num) {
@@ -37,14 +37,14 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  the global scope:
 
 function arrayEach(array, func) {
-  for (i = 0; i < array.length; i++) {
+  for ( var i = 0; i < array.length; i++) {
     func(array[i]);
   }
 }
 
 arrayEach(['red','green','blue'], console.log);
 
-console.log(i) // should be 'undefined', not 3
+//console.log(i) // should be 'undefined', not 3
 
 //  4. Explain why this function does not modify the global
 //  variable x declared on line 5 above. Write your explanation
